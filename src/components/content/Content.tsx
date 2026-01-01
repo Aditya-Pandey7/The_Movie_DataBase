130175e82e0d96c68b474962e1c35c928ccea62b
captain-adi <maniramp554@gmail.com>
2026-01-01 23:32:28 +0530

    feat: add trending background SVG and integrate it into Content component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header";
 import Scroller from "./components/scroller/Scroller";
+import trendingBG from "../../../public/trending-bg.svg";
 import type { IContentProps } from "./type";
 function Content<T>({
   value,
@@ -11,8 +12,13 @@ function Content<T>({
   const result = data?.results;
   console.log("result ", result);
   return (
-    <section className="py-7">
-      <div className="width-stack">
+    <section className="py-7 ">
+      <div
+        className="width-stack bg-no-repeat  bg-position-[50%_80%] "
+        style={{
+          backgroundImage: `url(${trendingBG})`,
+        }}
+      >
         <Header
           title={title}
           value={value}
