083a8aaba930cb03045be0f2f242925ab238b61e
captain-adi <maniramp554@gmail.com>
2026-02-02 18:32:38 +0530

    refactor: remove unnecessary React import in SeriesCast component

--- src/pages/detailedpage/components/contentWrapper/components/seriesCast/SeriesCast.tsx
+++ src/pages/detailedpage/components/contentWrapper/components/seriesCast/SeriesCast.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from "react";
+import { memo, useMemo } from "react";
 import type { ICreditsResponse } from "./type";
 import { Link, useParams } from "react-router-dom";
 
