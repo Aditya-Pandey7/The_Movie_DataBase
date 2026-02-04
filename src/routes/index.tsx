e2bb8a14122192c5c6b9d431ce0ed2c6f50ce3bd
captain-adi <maniramp554@gmail.com>
2026-02-04 16:31:02 +0530

    refactor: implement lazy loading for components in router configuration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,67 @@
 import { createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import App from "@/App";
-import Detailedpage from "@/pages/detailedpage/Detailedpage";
-import Hero from "@/pages/Hero/Hero";
-import CastNCrew from "@/pages/castNcrew/CastNCrew";
-import PersonDetails from "@/pages/personDetailes/PersonDetails";
+const SearchResult = lazy(() => import("@/pages/searchResult/SearchResult"));
+const LoadingDialog = lazy(
+  () => import("@/components/loadingDialog/LoadingDialog"),
+);
+const App = lazy(() => import("@/App"));
+const Detailedpage = lazy(() => import("@/pages/detailedpage/Detailedpage"));
+const Hero = lazy(() => import("@/pages/Hero/Hero"));
+const CastNCrew = lazy(() => import("@/pages/castNcrew/CastNCrew"));
+const PersonDetails = lazy(
+  () => import("@/pages/personDetailes/PersonDetails"),
+);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <Suspense fallback={<LoadingDialog open={true} />}>
+        <App />
+      </Suspense>
+    ),
     children: [
       {
         path: "/",
-        element: <Hero />,
+        element: (
+          <Suspense fallback={<div>Loading ....</div>}>
+            <Hero />
+          </Suspense>
+        ),
+        // action: homeAction,
       },
       {
         path: "/details/:type/:id",
-        element: <Detailedpage />,
+        element: (
+          <Suspense fallback={<div>Loading ....</div>}>
+            <Detailedpage />
+          </Suspense>
+        ),
       },
       {
         path: "/cast&crew/:type/:id/cast",
-        element: <CastNCrew />,
+        element: (
+          <Suspense fallback={<div>Loading ....</div>}>
+            <CastNCrew />
+          </Suspense>
+        ),
       },
       {
         path: "/person/:id",
-        element: <PersonDetails />,
+        element: (
+          <Suspense fallback={<div>Loading ....</div>}>
+            <PersonDetails />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/search",
+        element: (
+          <Suspense fallback={<div>Loading ....</div>}>
+            <SearchResult />
+          </Suspense>
+        ),
       },
     ],
   },
